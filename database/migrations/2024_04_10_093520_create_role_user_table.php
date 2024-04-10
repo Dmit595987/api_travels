36ac5f0db0578140df343801f94fb7298d01e0c1
dimax-otdelka <2957549@gmail.com>
2024-04-10 14:36:23 +0300

    сделано всё для Миграции UUID

--- database/migrations/2024_04_10_093520_create_role_user_table.php
+++ database/migrations/2024_04_10_093520_create_role_user_table.php
@@ -12,9 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('role_user', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('role_id')->constrained('roles');
-            $table->foreignId('user_id')->constrained('users');
+            $table->uuid('id')->primary();
+            $table->foreignUuid('role_id')->constrained('roles');
+            $table->foreignUuid('user_id')->constrained('users');
             $table->timestamps();
         });
     }
